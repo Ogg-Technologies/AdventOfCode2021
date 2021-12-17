e883be6b52db03973b3b94bd76e5edd43481fe77
Oskar <ogg.developer@gmail.com>
2021-12-17 16:52:10 +0100

    started prob17

---
+++ src/prob17/Prob17.kt
@@ -0,0 +1,13 @@
+package prob17
+
+fun main() {
+
+    for (startYSpeed in 0..1000) {
+        var ySpeed = startYSpeed
+        var yPos = 0
+        yPos += ySpeed
+        ySpeed -= 1
+
+        if (yPos in -75..-49)
+    }
+}
